396964eb523469254d557039da1f0bc7310b9d99
sekkta0 <159622566+sekkta0@users.noreply.github.com>
2024-03-04 17:46:34 +0000

    HW#5

---
+++ hello.py
@@ -0,0 +1,20 @@
+a1=5
+a2=50
+a3=500
+print('a1=',a1)
+print('a2=',a2)
+print('a3=',a3)
+b1=5
+b2=50
+b3=500
+print('b1=',b1)
+print('b2=',b2)
+print('b3=,'b3)
+('a1==b1':',a1==b1')
+('a2==b2':',a2==b2')
+('a3==b3':',a3==b3')
+a4=5000
+b4=5000
+print('a4==b4:',a4==b4)#a4==b4:True
+print('a4 is b4:',a4+0 is b4+0)#a4 is b4:True
+print('a4+1 is b4+1:',a4+1 is b4+1)#a4+1 is b4+1:Folse
